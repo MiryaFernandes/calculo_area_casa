d71e1c916b8acaf48f6d442f2956dbc8c756cdcc
MIRYÃ ELIZABETH FERNANDES BORGES <21276382@CFP127ALUNO.LOCAL>
2021-09-09 15:49:26 -0300

    projeto calculo de uma casa

--- src/calculo_casa/AreaDaCasa.java
+++ src/calculo_casa/AreaDaCasa.java
@@ -8,9 +8,9 @@ public class AreaDaCasa {
 			
          Scanner leitor = new Scanner (System.in);
          
-         String comodo, sim, nao;
+         String comodo1, comodo2;
                  	
-         int largura, comprimento, totalMetros;
+         int largura1, largura2, comprimento1, comprimento2, totalMetros, totalMetros2, totalCasa;
          
          System.out.println("--------------------------------");
          System.out.println("Calculo da area de uma casa");
@@ -18,20 +18,68 @@ public class AreaDaCasa {
          System.out.println();
          
          System.out.print("Qual o nome do cômodo? ");
-		 comodo = leitor.next();
+		 comodo1 = leitor.next();
 		 
 		 System.out.print("Qual a largura? ");
-	     largura = leitor.nextInt();
+	     largura1 = leitor.nextInt();
          	
 	     System.out.print("Qual o comprimento? ");
-	     comprimento = leitor.nextInt();
+	     comprimento1 = leitor.nextInt();
 	     
-	     totalMetros = (largura * comprimento);
-	     System.out.println("A(o) " + comodo + " tem " + totalMetros + " metros quadrados.");
-	
 	     System.out.println();
-	     System.out.println("Deseja calcular mais comôdos? ");
+	     totalMetros = (largura1 * comprimento1);
+	     System.out.println("A(o) " + comodo1 + " tem " + totalMetros + " metros quadrados.");
+	
+	     String resposta;
+	     
+	     System.out.println();
+	     System.out.print("Deseja calcular mais comôdos(S/N)? ");
+	     resposta = leitor.next();
+	     System.out.println();
+	     
+	     
+	     if (resposta.equals("sim")||resposta.equals("Sim")) {
+	    	 
+	    	 while (resposta.equals("sim")||resposta.equals("Sim")) {
+	    	 
+	    	 System.out.print("Qual o nome do cômodo? ");
+			 comodo2 = leitor.next();
+			 
+			 System.out.print("Qual a largura? ");
+		     largura2 = leitor.nextInt();
+	         	
+		     System.out.print("Qual o comprimento? ");
+		     comprimento2 = leitor.nextInt();
+		     System.out.println();
+		     
+		     
+		     totalMetros2 = (largura2 * comprimento2);
+		     System.out.print("A(o) " + comodo2 + " tem " + totalMetros2 + " metros quadrados.");
+		     
+		     System.out.println();
+		     totalCasa = (totalMetros + totalMetros2);
+		     System.out.println("A área total da casa é " + totalCasa + " metros quadrados");
+		     System.out.println();
+		     
+		     
+		     System.out.print("Deseja calcular mais comôdos(S/N)? ");
+		     resposta =leitor.next();
+		     System.out.println();
+	    	 
+	     } } else if (resposta.equals("nao")||resposta.equals("Nao")){
+		     
+	    	 
+	    	 System.out.println("* * * F I M * * *");
+	    	 
+	     } else {
+	    	 
+	    	 System.out.print("Não corresponde a uma resposta válida!");
+	    	 
+	     }
 	     
 	}
 
 }
+	
+
+
